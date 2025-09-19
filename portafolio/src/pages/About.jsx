055f6947dd0978719f991a1513e910189204a438
pilig <pilig.del07@gmail.com>
2025-09-18 21:03:56 -0600

    about page, contact page

--- portafolio/src/pages/About.jsx
+++ portafolio/src/pages/About.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+import { skills, experiences } from '../constants';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import CTA from '../components/CTA';
 
 export const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Pilar</span></h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Software Engineer based in Mexico, developing web and mobile applications with a focus on seamless user experiences.
+        </p>
+      </div>
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl' />
+              <div className='flex btn-front rounded-xl justify-center items-center'>
+                <img src={skill.imageUrl} alt={skills.name} className='w-1/2 h-1/2 object-contain'></img>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='py-16'>
+        <h3 className='subhead-text'>Work Experience</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+            I've been fortunate to contribute to projects across different industries, growing my expertise and teaming up with inspiring professionals. Here's an overview:
+          </p>
+        </div>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement
+                key={experience.company_name} date={experience.date} iconStyle={{ background: experience.iconBg }} contentStyle={{ borderBottom: '8px', borderStyle: 'solid', borderBottomColor: experience.iconBg, boxShadow: 'none' }} icon={<div className='flex justify-center items-center w-full h-full'> <img src={experience.icon} alt={experience.company_name} className='w-[60%] h-[60%] object-contain'></img></div>}>
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>{experience.title}</h3>
+                  <p style={{ margin: 0 }} className='text-black-500 font-medium font-base'>{experience.company_name}</p>
+                </div>
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li key={`experience-point-${index}`} className='text-black-500/50 font-normal pl-1 text-sm'>
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+
+      <hr className='border-slate-200'></hr>
+
+      <CTA></CTA>
+
+
+    </section>
   )
 }
 
--- portafolio/src/pages/Contact.jsx
+++ portafolio/src/pages/Contact.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { Suspense, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+import Fox from '../models/Fox';
+import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 export const Contact = () => {
+
+  const formRef = useRef(null);
+  const [form, setform] = useState({ name: '', email: '', message: '' });
+  const [currentAnimation, setcurrentAnimation] = useState('idle');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { alert, showAlert, hideAlert } = useAlert();
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value })
+  };
+
+
+  const handleFocus = () => {
+    setcurrentAnimation('walk');
+  };
+
+  const handleBlur = () => {
+    setcurrentAnimation('idle');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    setcurrentAnimation('hit');
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID, {
+      name: form.name,
+      to_name: "Pilar",
+      from_email: form.email,
+      to_email: import.meta.env.VITE_APP_EMAIL,
+      message: form.message
+    },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      showAlert({ show: true, text: "Message sent successfully! I'll be in contact soon", type: 'success' });
+      setTimeout(() => {
+        hideAlert();
+        setcurrentAnimation('idle')
+        setform({ name: '', email: '', message: '' });
+      }, [3000])
+
+
+    }).catch((error) => {
+      setIsLoading(false);
+      showAlert({ show: true, text: "I didn't receive your message", type: 'danger' })
+      setcurrentAnimation('idle');
+      console.log(error);
+    })
+  };
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row felx-col max-container '>
+      {alert.show && <Alert {...alert}></Alert>}
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch!</h1>
+        <form className='w-full flex flex-col gap-7 mt-10' onSubmit={handleSubmit}>
+          <label className='text-black-500 font-semibold'>Name</label>
+          <input type='text' name='name' required className='input' placeholder='John Doe' value={form.name} onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur}></input>
+          <label className='text-black-500 font-semibold'>Email</label>
+          <input type='email' name='email' required className='input' placeholder='john@gmail.com' value={form.email} onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur}></input>
+          <label className='text-black-500 font-semibold'>Your Message</label>
+          <textarea required name='message' className='textarea' rows={4} placeholder='Let me know how I can help you!' onChange={handleChange} value={form.message} onFocus={handleFocus} onBlur={handleBlur}></textarea>
+
+          <button type='submit' className='btn' onFocus={handleFocus} onBlur={handleBlur}> {isLoading ? 'Sending...' : 'Send Message'}</button>
+        </form>
+
+
+      </div>
+
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas camera={{ positon: [0, 0, 5], fov: 75, near: 0.1, far: 1000 }}>
+          <directionalLight intensity={2.5} position={[0, 0, 1]}></directionalLight>
+          <ambientLight intensity={0.5}></ambientLight>
+          <Suspense fallback={<Loader></Loader>}>
+            <Fox
+              currentAnimation={currentAnimation}
+              positon={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            ></Fox>
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
   )
 }
 
